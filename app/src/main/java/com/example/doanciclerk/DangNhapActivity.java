f8a0bb5b783ae5d4c63551d9ea0c2eb8dc7f02c4
LuuQuangKhai <117250965+LuuQuangKhai@users.noreply.github.com>
2023-04-15 08:16:47 +0700

    chua xong giao dien

--- app/src/main/java/com/example/doanciclerk/DangNhapActivity.java
+++ app/src/main/java/com/example/doanciclerk/DangNhapActivity.java
@@ -9,6 +9,6 @@ public class DangNhapActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dang_nhap);
+        setContentView(R.layout.signup_tap_fragment);
     }
 }
